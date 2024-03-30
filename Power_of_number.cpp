0da801363b47e159d85a04129bcd0fe877cd4cfa
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2024-03-30 21:00:35 +0530

    Add files via upload

---
+++ Power_of_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a Number: ";
+    cin>>n;
+
+    int p;
+    cout<<"Enter Power: ";
+    cin>>p;
+
+    cout<<"The Number is: "<<pow(n,p);
+}
