0cb051acb643fc3941fc0d6472c4f12d2d119508
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2024-03-31 00:10:50 +0530

    Add files via upload

---
+++ Digonal_Pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    cout<<"Enter number of Rows: ";
+    cin>>r;
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=r;j++){
+            if(i==j){
+                cout<<" * ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
