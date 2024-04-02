09da3313700d578130b046a9e91c0fd7e170c1e6
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2024-04-02 23:15:08 +0530

    Add files via upload

---
+++ 5Star_Pattern_Array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    string star[3]={"* * *", "  *  ", "* * *"};
+
+    cout<<star[0]<<endl;
+    cout<<star[1]<<endl;
+    cout<<star[2]<<endl;
+}
---
+++ Pattern_with_Array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    //Printing Pattern with the help of Array....
+
+    string num[4]={"1","0 1", "1 0 1","0 1 0 1"};
+    cout<<num[0]<<endl;
+    cout<<num[1]<<endl;
+    cout<<num[2]<<endl;
+    cout<<num[3]<<endl;
+    
+    return 0;
+}
